26d184ddd565050d56e9b3ccf5a131b39430bb9b
Benson <njugunab655@gmail.com>
2024-01-16 11:50:13 +0300

    Add getReview method

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -18,6 +18,24 @@ const getAllReviews = async (req, res) => {
   }
 }
 
+const getReview = async (req, res) => {
+  try {
+    const id = req.params
+    const review = await Review.findById(req.params.id);
+
+    // SEND RESPONSE
+    res.status(201).json({
+      status: 'Success',
+      message: review,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err.message
+    }) 
+  }
+}
+
 const addReview = async(req, res) => {
   try {
     // add a review
@@ -38,5 +56,6 @@ const addReview = async(req, res) => {
 
 module.exports = {
   getAllReviews,
+  getReview,
   addReview
 }
--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 // controllers
-const { getAllReviews, addReview } = require('../controllers/reviewsController');
+const { getAllReviews, addReview, getReview } = require('../controllers/reviewsController');
 
 console.log(getAllReviews)
 
 const router = express.Router();
 
-router.route('/').get(getAllReviews).post(addReview)
+router.route('/').get(getAllReviews).post(addReview);
+router.route('/:id').get(getReview);
 
 module.exports = router;
