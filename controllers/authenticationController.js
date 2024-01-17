a8365b58c049f870085b7b3af071c2ef640202a0
Benson <njugunab655@gmail.com>
2024-01-17 20:06:57 +0300

    Change the logic

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -6,9 +6,8 @@ const registerUser = async (req, res) => {
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     const password = await bcrypt.hash(req.body.password, salt);
-    const name = req.body.name;
-
-    await User.create({ name, password });
+    const info = {...req.body, password};
+    await User.create(info);
 
     res.status(201).json({
       status: 'Success',
