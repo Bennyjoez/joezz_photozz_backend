caf85c326c14a12a56fcb36bd1f3dd5ed22bd23a
Benson <njugunab655@gmail.com>
2024-01-18 09:30:45 +0300

    Fix bug

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getAllBookings, addBooking } = require('../controllers/bookingsController');
 
-const router = express.router();
+const router = express.Router();
 
 router.route('/').get(getAllBookings).post(addBooking)
 
