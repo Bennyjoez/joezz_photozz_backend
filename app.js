574eff6171ee9174510f24041b22c6d090c6c518
Benson <njugunab655@gmail.com>
2024-01-23 12:03:19 +0300

    Setup cors and global error handler

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Change this to your React app's URL in production
+  methods: 'GET,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //logs requests to the console e.g. GET /img/favicon.png 200 6.047 ms - 12789
 }
@@ -16,8 +24,7 @@ const requestTime = function (req, res, next) {
   next()
 }
 
-app.use(requestTime)
-app.use(cors());
+app.use(requestTime);
 
 app.use(express.json());
 
@@ -30,4 +37,11 @@ app.use('/api/v1/bookings', bookingsRouter);
 app.get('*', (req, res) => {
   res.status(404).send('Check the route and try again!! 🔴')
 })
+
+// global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 module.exports = app;
