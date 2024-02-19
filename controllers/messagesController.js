4bd5505be8e440e284c619155a352e625f1c78e9
Benson Njuguna <njugunab655@gmail.com>
2024-02-19 10:52:21 +0300

    Create an save message to DB

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,5 +1,23 @@
 // control all messages sent
 
+const Message = require("../models/messageModel")
+
+// handle errors
+const handleErrors = (err) => {
+  const fields = Object.keys(err.errors);
+  const errors = {};
+  if (err.name === 'ValidationError') {
+    fields.forEach(er => {
+      errors[er] = {
+        name: err.errors[er].name,
+        message: err.errors[er].message,
+      }
+    })
+    return errors;
+  }
+  return err
+}
+
 // get all messages
 const getAllMessages = async (req, res) => {
   res.status(201).json({
@@ -12,12 +30,21 @@ const getAllMessages = async (req, res) => {
 
 // add message
 const addMessage = async (req, res) => {
-  res.status(201).json({
-    status: 'Success',
-    data: {
-      message: 'Here are the messages'
-    }
-  })
+  try {
+    const message = await Message.create(req.body);
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        message: message
+      }
+    })
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(404).json({
+      status: 'Fail',
+      errors
+    }) 
+  }
 }
 
 module.exports = { getAllMessages, addMessage };
