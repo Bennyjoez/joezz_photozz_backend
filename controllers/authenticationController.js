ab992de0e19c7f759f80f3fda6a6bcedc43b54b3
Benson Njuguna <njugunab655@gmail.com>
2024-02-28 20:01:33 +0300

    Send usertoken as user details

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -39,12 +39,12 @@ const registerUser = async (req, res) => {
       id: user._id,
       name: user.name,
       email: user.email,
-      contact: user.contact
+      contact: user.contact,
+      userToken: token
     }
 
     res.status(201).json({
       status: 'Success',
-      token,
       data: { user: filteredUser },
       message: 'User registered!',
     });
@@ -89,12 +89,12 @@ const loginUser = async (req, res) => {
       id: user._id,
       name: user.name,
       email: user.email,
-      contact: user.contact
+      contact: user.contact,
+      userToken: token
     }
     // send successful response
     res.status(200).json({
       status: 'Success',
-      token,
       user: filteredUser
     });
   } catch (err) {
