75ccd65c409fcbec39126a517bdc24b23f4a6c6e
Benson Njuguna <njugunab655@gmail.com>
2024-03-18 11:11:59 +0300

    Find correct bookings per client id

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -3,73 +3,73 @@ const Booking = require("../models/bookingsModel");
 const handleErrors = (err) => {
   let errors = {};
 
-  if(err.message.includes('duplicate key error')) {
+  if (err.message.includes("duplicate key error")) {
     errors = {
       code: err.code,
-      message: `The date is already reserved. Choose a different date`
-    }
+      message: `The date is already reserved. Choose a different date`,
+    };
   }
 
   return errors;
-} 
+};
 
 const getAllBookings = async (req, res) => {
   try {
     // get all bookings with client data
-    const bookings = await Booking.find().populate({ 
-      path: 'client',
-      select: 'name'
+    const bookings = await Booking.find().populate({
+      path: "client",
+      select: "name",
     });
     // SEND RESPONSE
     res.status(200).json({
-      status: 'Success',
+      status: "Success",
       results: bookings.length,
       data: bookings,
     });
   } catch (err) {
     res.status(404).json({
-      status: 'Fail',
-      message: err.message
-    }) 
+      status: "Fail",
+      message: err.message,
+    });
   }
-}
+};
 
 const getBookingsById = async (req, res) => {
   try {
     const userId = req.params.id;
-    // get all bookings for a give Id
-    const bookings = await Booking.find().where('client').equals(userId);
+    // get all bookings for a given client
+    const bookings = await Booking.find({ client: userId });
     // SEND RESPONSE
     res.status(200).json({
-      status: 'Success',
+      status: "Success",
       results: bookings.length,
       data: bookings,
     });
   } catch (err) {
     res.status(404).json({
-      status: 'Fail',
-      message: err.message
-    }) 
+      status: "Fail",
+      message: err.message,
+    });
   }
-}
+};
 
 const addBooking = async (req, res) => {
   try {
     // create a booking
     const client = req.user._id;
-    const booking = await Booking.create({ ...req.body, client});
+    const booking = await Booking.create({ ...req.body, client });
     // SEND RESPONSE
     res.status(201).json({
-      status: 'Success',
-      booking
+      status: "Success",
+      booking,
     });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(404).json({
-      status: 'Fail',
-      errors
-    })
+      status: "Fail",
+      errors,
+    });
   }
-}
+};
 
-module.exports = { getAllBookings, getBookingsById, addBooking }
+module.exports = { getAllBookings, getBookingsById, addBooking };
