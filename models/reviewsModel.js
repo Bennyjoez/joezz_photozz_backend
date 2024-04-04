8d4862649e19cc3043afe4185093004327fe4d8b
Benson Njuguna <njugunab655@gmail.com>
2024-04-04 19:50:03 +0300

    Change type of rating in review

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -10,7 +10,7 @@ const schema = new mongoose.Schema({
     unique: false
   },
   rating: {
-    type: Number,
+    type: String,
     default: 4.5,
     required: [true, "A rating would be nice."]
   },
